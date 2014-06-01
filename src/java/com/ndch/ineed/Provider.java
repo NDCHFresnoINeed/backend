24c9b8ff68d7f973a088a0bb5a039b87a5791076
Bipul Karki <bipskarki@gmail.com>
2014-05-31 19:04:32 -0700

    Added Location and ServiceHour classes.

--- src/java/com/ndch/ineed/Provider.java
+++ src/java/com/ndch/ineed/Provider.java
@@ -1,9 +1,59 @@
 package com.ndch.ineed;
 
+import java.util.Map;
+
 public class Provider {
 	private String name;
+	private Location location;
+	private String phoneNumber;
+	private Map <String, ServiceHour> serviceHours;
 	
-	public Provider(){
-		name = "bob";
+	
+	
+	public Location getLocation() {
+		return location;
 	}
+
+
+
+	public void setLocation(Location location) {
+		this.location = location;
+	}
+
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+
+
+	public Map <String, ServiceHour> getServiceHours() {
+		return serviceHours;
+	}
+
+
+
+	public void setServiceHours(Map <String, ServiceHour> serviceHours) {
+		this.serviceHours = serviceHours;
+	}
+
+
 }
---
+++ src/java/com/ndch/ineed/ServiceHour.java
@@ -0,0 +1,9 @@
+package com.ndch.ineed;
+
+//import java.util.Date;
+
+public class ServiceHour {
+private String openingHour;
+private String closingHour;
+
+}
